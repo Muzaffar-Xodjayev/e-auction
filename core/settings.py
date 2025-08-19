99a18ed37819bcda0c42a42bf1e3dc54eb32fcab
EC2 Default User <ec2-user@ip-172-31-34-174.ec2.internal>
2025-08-19 06:22:18 +0000

    update

--- core/settings.py
+++ core/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#if DEBUG:
+#    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#else:
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
