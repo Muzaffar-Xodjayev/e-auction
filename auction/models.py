132882253550e37ca905108a8bb0c01d09f0edd9
Muzaffar <khodjayevmuzaffar@gmail.com>
2025-08-25 11:33:47 +0500

    update

--- auction/models.py
+++ auction/models.py
@@ -27,6 +27,7 @@ class Comment(models.Model):
     listing_id = models.ForeignKey(Listing, models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.CharField(max_length=255)
+    test = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return str(self.author)
